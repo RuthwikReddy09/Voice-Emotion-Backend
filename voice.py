42eb2e3847fada25005ecdfe05f4e97c41aaad31
ruthwik09 <druthwik09@gmail.com>
2024-02-21 11:58:18 +0530

    added 5 second feature

--- voice.py
+++ voice.py
@@ -1,10 +1,7 @@
-# Import libraries
-import librosa # for audio processing
-import numpy as np # for numerical operations
-import tensorflow as tf # for deep learning
+import librosa 
+import numpy as np 
+import tensorflow as tf 
 from flask_cors import CORS
-# Load the audio file and extract features
-
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -20,21 +17,21 @@ def upload_audio():
         return jsonify({'error': 'No audio file provided'}), 400
     
     audio_file = request.files['audio']
-
     audio_file.save('audio.mp3')
-    # y, sr = librosa.load("audio.wav", duration=5.0, offset=0.5)
     y, sr = librosa.load("C:\\Users\\druth\\Desktop\\stress detection\\audio.mp3", duration=5.0)
     mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T, axis=0)
-    mfcc = np.expand_dims(mfcc, axis=-1) # add a channel dimension
-    mfcc = np.expand_dims(mfcc, axis=0) # add a batch dimension
+    mfcc = np.expand_dims(mfcc, axis=-1)  
+    mfcc = np.expand_dims(mfcc, axis=0) 
 
     emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Surprise', 'Sad']
     emotion_model = tf.keras.models.load_model('C:\\Users\\druth\\Desktop\\stress detection\\Voice-Emotion-Backend\\model.h5') # load the model
-    predictions = emotion_model.predict(mfcc) # make predictions
-    emotion = np.argmax(predictions) # get the index of the highest probability
+    predictions = emotion_model.predict(mfcc) 
+    emotion = np.argmax(predictions)
     print(f'The emotion of the audio file is {emotion_labels[emotion]}.')
     return jsonify(emotion_labels[emotion]), 200
 
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
+
+    
